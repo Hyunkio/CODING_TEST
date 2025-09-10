b377afd36bc137a848371332880438d7e67d4227
Hyeon Gi Oh <202221054@sangmyung.kr>
2025-09-10 16:59:05 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 396 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -0,0 +1,13 @@
+import sys
+
+def equation():
+    a, b, c, d, e, f = map(int, sys.stdin.readline().split())
+    
+    for x in range(-999, 1000):
+        for y in range(-999, 1000):
+            
+            if(a * x + b * y == c) and (d * x + e * y == f):
+                print(x, y)
+                return
+
+equation()
